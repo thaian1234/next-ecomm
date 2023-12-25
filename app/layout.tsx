ba54b3046c0d3a4c157b4dac0f7373ba231a8938
thaian1234 <121446197+thaian1234@users.noreply.github.com>
2023-12-25 16:14:05 +0700

    install package and set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ModalProvider } from "@/providers/modal-provider";
 import { ToasterProvider } from "@/providers/toast-provider";
 
 import "./globals.css";
+import ReactQueryProvider from "@/providers/react-query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,9 +23,11 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang="en">
 				<body className={inter.className}>
-					<ToasterProvider />
-					<ModalProvider />
-					{children}
+					<ReactQueryProvider>
+						<ToasterProvider />
+						<ModalProvider />
+						{children}
+					</ReactQueryProvider>
 				</body>
 			</html>
 		</ClerkProvider>
